3c36924d8d57dc1036d1d8bd5a60afa1301030fc
Yisu12 <81430614+Yisu12@users.noreply.github.com>
2022-02-22 00:08:07 +0000

    v1 Buscador Gifs y Stickers
    Aplicación finalizada y desplegada.

---
+++ precache-manifest.bc39a3d68c6fc98fdf77936ada73f7ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31671799d57b1b0493b",
+    "url": "/css/app.e1cfae5a.css"
+  },
+  {
+    "revision": "32e2a63fa069ae9b7541",
+    "url": "/css/gifs.f3b6400a.css"
+  },
+  {
+    "revision": "ad5242e0dd8e33f4539e",
+    "url": "/css/stickers.fe931b7e.css"
+  },
+  {
+    "revision": "f5cf753c2a707222b702c4ed02f48d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ac9d87ebefc9c8477d",
+    "url": "/js/about.00eed510.js"
+  },
+  {
+    "revision": "b31671799d57b1b0493b",
+    "url": "/js/app.5c1730d9.js"
+  },
+  {
+    "revision": "ce082a0f02296ee16419",
+    "url": "/js/chunk-vendors.6699cfb7.js"
+  },
+  {
+    "revision": "32e2a63fa069ae9b7541",
+    "url": "/js/gifs.daba6320.js"
+  },
+  {
+    "revision": "81c8487b3fe8412972de",
+    "url": "/js/gifs~stickers.49a46295.js"
+  },
+  {
+    "revision": "ad5242e0dd8e33f4539e",
+    "url": "/js/stickers.7a6c03e9.js"
+  },
+  {
+    "revision": "edb846ee1852bb9cc4cc3094bb5a24ec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
